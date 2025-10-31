e5f5e0e02a52c890a61f9530b2b8be1888ec40bf
Hillary Tanui <hillarytanui80@gmail.com>
2025-10-31 13:34:36 +0300

    completed assignment

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-// Your code here
+// Your tests here
+export function isPalindrome(word) {
+
+    word = word.toLowerCase();
+    
+    // Remove spaces using regex
+    word = word.replace(/\s/g, '');
+
+    return word === word.split('').reverse().join('');
+  }
